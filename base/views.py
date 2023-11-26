113ea5e735eabd3f18d10e104bc36c07a5944d5c
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-11-26 12:13:37 +0000

    render html

--- base/views.py
+++ base/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
+
+
 def home(request):
-    return HttpResponse("Hello, home.")
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse("Hello, ridwan")
+    return render(request, 'about.html')
