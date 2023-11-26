9a67ec45282876b7b8fcda706b4371b577edc7a5
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-11-26 12:46:57 +0000

    adds abouts json data

--- base/views.py
+++ base/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+abouts = [
+    {
+        'id': 1,
+        'name': 'Ridwan Halim',
+        'title': 'Python Developer',
+        'description': 'I am a developer who loves to solve problems and create beautiful and functional applications. I am a student at University of Technology Yogyakarta and I am currently learning Python.'
+    },
+    {
+        'id': 2,
+        'name': 'hafidhah Afkariana',
+        'title': 'Law',
+        'description': 'I am a lawyer who loves to solve problems and create beautiful and functional applications.'
+    },
+    {
+        'id': 3,
+        'name': 'hapidoo',
+        'title': 'Web Developer',
+        'description': 'I am a web developer who loves to solve problems and create beautiful and functional applications'
+    }
+]
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'abouts': abouts}
+    return render(request, 'home.html', context)
 
 def about(request):
     return render(request, 'about.html')
