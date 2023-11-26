78ec1859d44d661711011822039ff4be3897dd77
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-11-26 10:58:31 +0000

    views and urls

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Hello, home.")
-
-def about(request):
-    return HttpResponse("Hello, ridwan")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    
+    path('', include('base.urls'))
 ]
