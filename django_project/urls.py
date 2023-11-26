aa80ae84934a06567912763235fe204a1a7bb5b5
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-11-26 10:49:20 +0000

    simple urls

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, home.")
+
+def about(request):
+    return HttpResponse("Hello, ridwan")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    
 ]
