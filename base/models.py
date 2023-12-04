a60c82eee4644ff548ea07a8db0a571b97b2c9f2
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-12-04 03:26:01 +0000

    Adds table named Project

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='static/img/projects/')
+    url = models.URLField()
+    github_url = models.URLField()
+    demo_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
