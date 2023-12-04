356db659ca56d7bd4962a7f25255833583ac080f
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-12-04 03:30:20 +0000

    comment unused code

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
+# from django.conf.urls.static import static
+# from django.conf import settings
 
 
 urlpatterns = [
@@ -14,5 +14,5 @@ urlpatterns = [
 ]
 
 # Serve media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
