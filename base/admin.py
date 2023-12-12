efed7f4d5d31ede4a783d75036683a674d58ff82
ridwaanhall <erbyl14@gmail.com>
2023-12-12 17:07:29 +0700

    adds register for new tables

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project
+from .models import Project, Skills, Sidebar, Home, About, Education
 
 admin.site.register(Project)
+
+admin.site.register(Skills)
+
+admin.site.register(Sidebar)
+
+admin.site.register(Home)
+
+admin.site.register(About)
+
+admin.site.register(Education)
