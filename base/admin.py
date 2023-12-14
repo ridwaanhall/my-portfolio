341a182c1821e85a575ecbb6f505f457f1e3380e
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-12-14 10:05:54 +0000

    registered certificate model

--- base/admin.py
+++ base/admin.py
@@ -1,18 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project, Skill, Sidebar, Home, About, Education, Logo
+from .models import Project, Skill, Sidebar, Home, About, Education, Logo, Certificate
 
 admin.site.register(Project)
-
 admin.site.register(Skill)
-
 admin.site.register(Sidebar)
-
 admin.site.register(Home)
-
 admin.site.register(About)
-
 admin.site.register(Education)
-
 admin.site.register(Logo)
+admin.site.register(Certificate)
