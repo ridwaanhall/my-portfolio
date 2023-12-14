df81286921e04bdd1e2d02dd510c8224fba64f85
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-12-14 11:41:02 +0000

    adds message form

---
+++ base/forms.py
@@ -0,0 +1,8 @@
+# forms.py
+from django import forms
+from .models import Message
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['name', 'email', 'message']
