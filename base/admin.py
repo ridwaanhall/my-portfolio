a15607431b3dbc78fefd4826b0fc00551f07a375
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-12-16 04:44:04 +0000

    registered the credential

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project, Skill, Sidebar, Home, About, Education, Logo, Certificate, Message
+from .models import Project, Skill, Sidebar, Home, About, Education, Logo, Certificate, Message, Credential
 
 admin.site.register(Project)
 admin.site.register(Skill)
@@ -12,3 +12,4 @@ admin.site.register(Education)
 admin.site.register(Logo)
 admin.site.register(Certificate)
 admin.site.register(Message)
+admin.site.register(Credential)
