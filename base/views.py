a0bb78daa1510d4488d70313acd01221d2700183
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-12-17 04:34:28 +0000

    deleted commented code and unused code

--- base/views.py
+++ base/views.py
@@ -5,43 +5,10 @@ import requests, os, json
 from .models import Sidebar, Home, Project, Education, About, Skill, Message, Credential
 from datetime import datetime, timedelta
 from statistics import mean
-# from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
-abouts = [
-    {
-        'id': 1,
-        'name': 'Ridwan Halim',
-        'title': 'Python Developer',
-        'description': 'I am a developer who loves to solve problems and create beautiful and functional applications. I am a student at University of Technology Yogyakarta and I am currently learning Python.'
-    }
-]
-
-# def github_activity(request):
-#     github_username = 'ridwaanhall'
-#     github_token = os.getenv('GITHUB_TOKEN')
-#     api_url = f'https://api.github.com/users/{github_username}/events'
-
-#     headers = {
-#         'Authorization': f'Bearer {github_token}',
-#         'Accept': 'application/vnd.github.v3+json',
-#     }
-
-#     response = requests.get(api_url, headers=headers)
-
-#     # Check if the request was successful (status code 200)
-#     if response.status_code == 200:
-#         # Convert the response data to JSON
-#         data = response.json()
-#         # Return a JsonResponse with the fetched data
-#         return JsonResponse(data, safe=False)
-#     else:
-#         # If the request was not successful, return an error response
-#         return JsonResponse({'error': 'Failed to fetch GitHub data'}, status=response.status_code)
-
-
 def github_activity(request):
     username = "ridwaanhall"
     access_token = os.getenv("GITHUB_SECRET")
@@ -77,69 +44,16 @@ def github_activity(request):
         "Authorization": "Bearer %s" % access_token,
         "Content-Type": "application/json",
     }
-    # mMBj-1pt0BagssdPKWNJvT_-jFeb5DuSD8a7iSM2Rdw
     data = json.dumps({"query": query})
 
-    # print(os.getenv("GITHUB_SECRET"))
-
     response = requests.post(api_url, headers=headers, data=data)
-
-    # Check if the request was successful (status code 200)
     if response.status_code == 200:
         response_data = response.json()
-        # Return a JsonResponse with the fetched data
         return JsonResponse(response_data, safe=False)
     else:
         # If the request was not successful, return an error response
         return JsonResponse({'error': 'Failed to fetch GitHub data'}, status=response.status_code)
 
-
-def fetch_github_activity(username):
-    access_token = os.getenv("GITHUB_ACCESS_TOKEN")
-    api_url = "https://api.github.com/graphql"
-
-    query = """
-      query {
-        user(login: "%s") {
-          contributionsCollection {
-            contributionCalendar {
-              colors
-              totalContributions
-              months {
-                firstDay
-                name
-                totalWeeks
-              }
-              weeks {
-                firstDay
-                contributionDays {
-                  color
-                  contributionCount
-                  date
-                }
-              }
-            }
-          }
-        }
-      }
-    """ % username
-
-    headers = {
-        "Authorization": "Bearer %s" % access_token,
-        "Content-Type": "application/json",
-    }
-
-    data = json.dumps({"query": query})
-
-    response = requests.post(api_url, headers=headers, data=data)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        response_data = response.json()
-        return response_data
-    else:
-        return {'error': 'Failed to fetch GitHub data!'}
-    
 
 def home(request):
     sidebar_data = Sidebar.objects.first()
@@ -156,39 +70,6 @@ def home(request):
     
     return render(request, 'base/home.html', context)
 
-def dashboard_ahehe(request):
-    # about = None
-    # for i in abouts:
-    #     if i['id'] == int(pk):
-    #         about = i
-    # context = {'about': about}
-
-    response = requests.get("https://my-portfolio.ridwaanhall.repl.co/github-activity/")
-
-    # Get JSON data
-    data = json.loads(response.text)
-    monthly_counts = {}
-    for month in data['data']['user']['contributionsCollection']['contributionCalendar']['months']:
-        year_month = f"{month['firstDay'][:7]}"
-        total_for_month = 0
-
-        for week in data['data']['user']['contributionsCollection']['contributionCalendar']['weeks']:
-            if week['firstDay'].startswith(year_month):
-                for day in week['contributionDays']:
-                    total_for_month += day['contributionCount']
-
-        monthly_counts[year_month] = total_for_month
-    
-    sidebar_data = Sidebar.objects.first()
-
-    context = {
-        'sidebar_data': sidebar_data,
-        'monthly_counts': monthly_counts,
-        'monthly_contributions': list(monthly_counts.keys())[::-1],
-        'total_contributions': list(monthly_counts.values())[::-1],
-    }
-    return render(request, 'base/dashboard.html', context)
-
 
 def format_date(date_str):
     if date_str is not None:
@@ -197,6 +78,7 @@ def format_date(date_str):
     else:
         # Handle the case where date_str is None
         return 'Invalid Date'  # or any other appropriate default value
+
 
 def dashboard(request):
     sidebar_data = Sidebar.objects.first()
@@ -220,7 +102,6 @@ def dashboard(request):
     for week in data['data']['user']['contributionsCollection']['contributionCalendar']['weeks']:
         for day in week['contributionDays']:
             date_str = day['date']
-            # date_obj = datetime.strptime(date_str, '%Y-%m-%d')
 
             daily_contributions.append({
                 "date": date_str,
@@ -310,6 +191,7 @@ def dashboard(request):
 
     return render(request, 'base/dashboard.html', context)
 
+    
 def project(request):
 
     sidebar_data = Sidebar.objects.first()
@@ -324,8 +206,8 @@ def project(request):
     
     return render(request, 'base/project.html', context)
 
+
 def certificate(request):
-    # credentials = Credential.objects.order_by('-created_at')
     credentials = Credential.objects.all().values()
     credentials_list = list(credentials)
     sidebar_data = Sidebar.objects.first()
@@ -351,11 +233,11 @@ def certificate(request):
     
     return render(request, 'base/certificate.html', context)
 
+
 def about(request):
     sidebar_data = Sidebar.objects.first()
     abouts = About.objects.first()
     latest_educations = Education.objects.order_by('-created_at')[:2]
-    # skills = Skill.objects.order_by('-created_at')
     skills_1 = Skill.objects.all().order_by('?')
     skills_2 = Skill.objects.all().order_by('?')
     skills_3 = Skill.objects.all().order_by('?')
@@ -372,15 +254,7 @@ def about(request):
     
     return render(request, 'base/about.html', context)
 
-# def contact(request):
 
-#     sidebar_data = Sidebar.objects.first()
-
-#     context = {
-#         'sidebar_data': sidebar_data,
-#     }
-    
-#     return render(request, 'base/contact.html', context)
 def contact(request):
     success_message = None
 
@@ -395,7 +269,6 @@ def contact(request):
         success_message = f'Message sent successfully! with name {name} and email {email}'
         
         return redirect('contact')
-
 
     sidebar_data = Sidebar.objects.first()
 
@@ -416,15 +289,11 @@ def playground(request):
     
     return render(request, 'base/playground.html', context)
 
+
 def loginPage(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-
-        # try:
-        #     user = User.objects.get(username=username)
-        # except:
-        #     messages.error(request, 'User does not exist')
 
         user = authenticate(request, username=username, password=password)
 
@@ -436,6 +305,7 @@ def loginPage(request):
             return redirect('login')
             
     return render(request, 'base/login.html')
+
 
 def logoutPage(request):
     logout(request)
