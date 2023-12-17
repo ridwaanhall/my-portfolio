67982eab39182d33052f20cb4c9a7ce8b2eeb026
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-12-17 04:36:27 +0000

    tidy up the code

--- base/models.py
+++ base/models.py
@@ -9,7 +9,6 @@ class Project(models.Model):
     skill_1 = models.CharField(max_length=255, blank=True)
     skill_2 = models.CharField(max_length=255, blank=True)
     skill_3 = models.CharField(max_length=255, blank=True)
-    # url = models.URLField()
     github_url = models.URLField()
     demo_url = models.URLField()
     date_start = models.CharField(max_length=255, blank=True)
@@ -19,7 +18,8 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
-    
+
+
 # user data sidebar model. name and username
 class Sidebar(models.Model):
     name = models.CharField(max_length=255)
@@ -30,7 +30,8 @@ class Sidebar(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+
 # create database firstname, role (such as student, web developer), based (the location of the user), shorttext (is a short description. ).
 class Home(models.Model):
     firstname = models.CharField(max_length=255)
@@ -42,6 +43,7 @@ class Home(models.Model):
 
     def __str__(self):
         return self.firstname
+
 
 # create about database. such as long text.
 class About(models.Model):
@@ -55,6 +57,7 @@ class About(models.Model):
         else:
             return self.longtext
 
+
 class Logo(models.Model):
     logo = models.ImageField(upload_to='static/img/logos/')
     logo_name = models.CharField(max_length=255)
@@ -63,6 +66,7 @@ class Logo(models.Model):
 
     def __str__(self):
         return self.logo_name
+
 
 class Education(models.Model):
     logo = models.URLField()
@@ -79,7 +83,8 @@ class Education(models.Model):
 
     def __str__(self):
         return self.name_education
-    
+
+
 class Skill(models.Model):
     # text and skill name
     text_label = models.CharField(max_length=255)
@@ -90,6 +95,7 @@ class Skill(models.Model):
     
     def __str__(self):
         return self.name_skill
+
 
 class Certificate(models.Model):
     img_org = models.URLField()
@@ -102,9 +108,9 @@ class Certificate(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
-    
     def __str__(self):
         return self.title
+
 
 class Credential(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -120,6 +126,7 @@ class Credential(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Message(models.Model):
     name = models.CharField(max_length=255)
