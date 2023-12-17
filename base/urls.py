20a3125750fdcb522166b324c39f3b35288eaff8
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-12-17 04:35:20 +0000

    deleted unused code and comment

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-# from django.conf.urls.static import static
-# from django.conf import settings
 
 
 urlpatterns = [
@@ -17,7 +15,3 @@ urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutPage, name='logout'),
 ]
-
-# Serve media files during development
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
