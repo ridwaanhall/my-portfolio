ba303238e122156dc093b997d20e021af277ef0b
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2024-01-26 12:18:08 +0000

    Update URL base

--- base/management/commands/update_urls.py
+++ base/management/commands/update_urls.py
@@ -1,6 +1,10 @@
 from django.core.management.base import BaseCommand
 from base.models import Credential
 
+'''
+python manage.py update_urls
+'''
+
 class Command(BaseCommand):
     help = 'Update URLs in the Credential model'
 
@@ -8,8 +12,9 @@ class Command(BaseCommand):
         credentials = Credential.objects.all()
 
         for credential in credentials:
-            old_url = "https://my-portfolio.ridwaanhall.repl.co"  # Old URL to be replaced
-            new_url = "https://aaff0b3f-edba-4302-84e4-4c21fe434e72-00-2jg27tl1pjs72.global.replit.dev"  # New URL
+            #old_url_old = "https://my-portfolio.ridwaanhall.repl.co"  # Old URL to be replaced
+            old_url = "https://aaff0b3f-edba-4302-84e4-4c21fe434e72-00-2jg27tl1pjs72.global.replit.dev"  # old
+            new_url = 'https://aaff0b3f-edba-4302-84e4-4c21fe434e72-00-18ppstrxi56pp.worf.replit.dev/'
 
             if old_url in credential.company_logo:
                 credential.company_logo = credential.company_logo.replace(old_url, new_url)
