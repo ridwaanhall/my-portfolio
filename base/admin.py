56100b2b143fabf8e26b7ad46fc2f69a86c15c5b
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2024-07-12 14:21:26 +0000

    career, responsibilities, career skills register

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project, Skill, Sidebar, Home, About, Education, Logo, Certificate, Message, Credential, Quote
+from .models import Project, Skill, Sidebar, Home, About, Education, Logo, Certificate, Message, Credential, Quote, Career, Responsibility, CareerSkill
 
 admin.site.register(Project)
 admin.site.register(Skill)
@@ -14,3 +14,24 @@ admin.site.register(Certificate)
 admin.site.register(Message)
 admin.site.register(Credential)
 admin.site.register(Quote)
+
+# @admin.register(Career)
+# class CareerAdmin(admin.ModelAdmin):
+#     list_display = ('position', 'company_name', 'location', 'start_date', 'end_date', 'current', 'employment_type', 'location_type')
+#     search_fields = ('company_name', 'position', 'location')
+#     list_filter = ('employment_type', 'location_type', 'start_date', 'end_date', 'current')
+
+
+class ResponsibilityInline(admin.TabularInline):
+    model = Responsibility
+    extra = 1
+
+class CareerSkillInline(admin.TabularInline):
+    model = CareerSkill
+    extra = 1
+
+class CareerAdmin(admin.ModelAdmin):
+    inlines = [ResponsibilityInline, CareerSkillInline]
+
+admin.site.register(Career, CareerAdmin)
+admin.site.register(Responsibility)
